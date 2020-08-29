281f7c83781b20789f4b8a6e52148c8cf1f64661
benny alva <bennytescha@gmail.com>
2020-08-29 18:18:37 -0500

    docs

---
+++ docs/precache-manifest.497c032fabf5462d20bac4df9edc4efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603d2a4ecf552eccbf55074556e56567",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e03565af56fe0989e72",
+    "url": "/static/css/main.5d00cddb.chunk.css"
+  },
+  {
+    "revision": "02f0463e3256ce279281",
+    "url": "/static/js/2.a9fb6f27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9fb6f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e03565af56fe0989e72",
+    "url": "/static/js/main.44362889.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
